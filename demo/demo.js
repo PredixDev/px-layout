3eef7e70699d8fdb3e3d38b67c227351b9bbee6b
Jonnie Spratley <jonnie.spratley@ge.com>
2016-06-17 13:27:32 -0700

    fix: updated demos

--- demo/demo.js
+++ demo/demo.js
@@ -45,3 +45,21 @@ function createDemoItems(count) {
   }
   return _out;
 }
+
+
+function createToc(id) {
+  var toc = document.getElementById(id);
+  var headings = document.querySelectorAll('h2'),
+    heading, link, li;
+  for (var i = 0; i < headings.length; i++) {
+    heading = headings[i].textContent;
+    headings[i].id = headings[i].textContent.replace(/\W/g, '-');
+    li = document.createElement('li');
+    link = document.createElement('a');
+    link.href = '#' + headings[i].id;
+    link.textContent = heading;
+    li.appendChild(link);
+    toc.appendChild(li);
+    console.log('Adding id to heading', headings[i]);
+  }
+}
