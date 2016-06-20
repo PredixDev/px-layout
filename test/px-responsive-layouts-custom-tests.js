e1543f02a6e7a30053a3cecc1c4ef47ec12b7f14
Jonnie Spratley <jonnie.spratley@ge.com>
2016-06-20 10:40:11 -0700

    fix: updated travis and layouts

--- test/px-responsive-layouts-custom-tests.js
+++ test/px-responsive-layouts-custom-tests.js
@@ -1,20 +1,46 @@
 /* global assert, suite, test*/
 // This is the wrapper for custom tests, called upon web components ready state
 function runCustomTests() {
-  // Place any setup steps like variable declaration and initialization here
 
-  // This is the placeholder suite to place custom tests in
-  // Use testCase(options) for a more convenient setup of the test cases
-  suite('<px-responsive-layouts>', function () {
-    var element = document.getElementById('px_layout_1');
-
+  suite('<px-layout>', function () {
+    var element = document.getElementById('fixture');
     test('renders', function () {
       assert.ok(element);
     });
 
-    test('element has reference to drawer', function () {
-      assert.ok(element.drawer);
+
+
+    suite('<px-drawer-layout>', function () {
+      var element = document.getElementById('px_layout_1');
+      test('renders', function () {
+        assert.ok(element);
+      });
+      test('has reference to px-drawer', function () {
+        assert.ok(element.drawer);
+      });
+
+      test('element is [drawer-toggle] when tapped toggles px-drawer', function () {
+        assert.ok(element.drawer);
+      });
     });
+
+
+    suite('<px-header-layout>', function () {
+      var element = document.getElementById('px_layout_2');
+
+      test('renders', function () {
+        assert.ok(element);
+      });
+
+      test('has reference to px-header', function () {
+        assert.ok(element.header);
+      });
+
+      test('has reference to px-footer', function () {
+        assert.ok(element.footer);
+      });
+    });
+
 
   });
 }
