826acbfab0ef9d1109c7b0060fa3d154d088374f
Jonnie Spratley <jonnie.spratley@ge.com>
2016-07-01 16:09:21 -0700

    fix: updated travis

--- wct.conf.js
+++ wct.conf.js
@@ -2,16 +2,24 @@ module.exports = {
 	verbose: true,
 	plugins: {
 		local: {
-			browsers: ['chrome']
+			browsers: ['chrome', 'firefox']
 		},
 		sauce: {
-			disabled: true
+			name: 'px-layout',
+			disabled: false,
+			browsers: [
+        {
+          'browserName': 'safari',
+          'version': '9'
+        }
+      ]
 		},
 		istanbul: {
 			'dir': './coverage',
 			'reporters': ['text-summary', 'lcov'],
 			'include': [
 				'/px-drawer-layout/px-*.html',
+				'/px-media-query/px-*.html',
 				'/px-header-layout/px-*.html',
 				'/px-layout/px-*.html',
 				'/px-*.html'
